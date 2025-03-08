39472e3d76d6a4bac61002f4ba9c30d114cfb0cf
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-03-08 23:02:27 +0800

    read from csv file

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	questionsAndAnswers := make(map[string]string)
+
+	for _, row := range data {
+		questionsAndAnswers[row[0]] = row[1]
+	}
+
+	quiz(&questionsAndAnswers)
+}
+
+func quiz(questionsAndAnswers *map[string]string) (correct, wrong int) {
+	correct = 0
+	wrong = 0
+
+	for key, value := range *questionsAndAnswers {
+		fmt.Println(key, " ", value)
+	}
+
+	return correct, wrong
 }
