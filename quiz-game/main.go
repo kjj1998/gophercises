9a59c78253143cc30570ae830673c9573d682c88
Koh Jun Jie <koh.jun.jie@outlook.com>
2025-03-10 22:49:05 +0800

    added timer

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -6,10 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	csvFilePtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFilePtr)
@@ -24,36 +26,42 @@ func main() {
 		panic(err)
 	}
 
-	correct, wrong := quiz(&data)
+	correct := quiz(&data, *limitPtr)
 
 	fmt.Printf("Number of questions answered correctly: %d\n", correct)
-	fmt.Printf("Number of questions answered wrongly: %d\n", wrong)
+	fmt.Printf("Total number of questions: %d\n", len(data))
 }
 
-func quiz(questionsAndAnswers *[][]string) (correct, wrong int) {
-	correct = 0
-	wrong = 0
+func quiz(questionsAndAnswers *[][]string, limit int) (correct int) {
+	fmt.Printf("Press Enter to start the quiz, you have %d seconds", limit)
+	reader := bufio.NewReader(os.Stdin)
+	_, _ = reader.ReadString('\n')
 
+	correct = 0
 	scanner := bufio.NewScanner(os.Stdin)
+	answerCh := make(chan string)
+	end := time.After(time.Duration(limit) * time.Second)
 
 	for index, row := range *questionsAndAnswers {
 		fmt.Printf("Problem #%d: %s = ", index+1, row[0])
 
-		scanner.Scan()
-		err := scanner.Err()
+		go getUserInput(answerCh, scanner)
 
-		if err != nil {
-			panic(err)
-		}
-
-		answer := scanner.Text()
-
-		if answer == row[1] {
-			correct++
-		} else {
-			wrong++
+		select {
+		case <-end:
+			fmt.Println("\nTime's Up!")
+			return correct
+		case answer := <-answerCh:
+			if answer == row[1] {
+				correct++
+			}
 		}
 	}
 
-	return correct, wrong
+	return correct
+}
+
+func getUserInput(answerCh chan string, scanner *bufio.Scanner) {
+	scanner.Scan()
+	answerCh <- scanner.Text()
 }
